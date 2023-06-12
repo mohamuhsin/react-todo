d71f66c1180d1f7765198ede3c2c5cada51483a5
mohamuhsin <muhsinohoho@outlook.com>
2023-06-12 14:11:49 +0300

    built card component and styled it

--- src/components/AddToDo/AddToDo.js
+++ src/components/AddToDo/AddToDo.js
@@ -1,19 +1,23 @@
 import React from "react";
 
+import Card from "../UI/Card";
+
 const AddToDo = (props) => {
   const AddToDoHandler = (event) => {
     event.preventDefault();
   };
   return (
-    <form onSubmit={AddToDoHandler}>
-      <label htmlFor="number">No.</label>
-      <input id="no." type="number" />
-      <label htmlFor="title">Title</label>
-      <input id="title" type="text" />
-      <label htmlFor="description">Description</label>
-      <input id="description" type="text" />
-      <button type="submit">Add To Do</button>
-    </form>
+    <Card>
+      <form onSubmit={AddToDoHandler}>
+        <label htmlFor="number">No.</label>
+        <input id="no." type="number" />
+        <label htmlFor="title">Title</label>
+        <input id="title" type="text" />
+        <label htmlFor="description">Description</label>
+        <input id="description" type="text" />
+        <button type="submit">Add To Do</button>
+      </form>
+    </Card>
   );
 };
 export default AddToDo;
