f69d14c6bdf92c26b5c641298593050ab68067ed
mohamuhsin <muhsinohoho@outlook.com>
2023-06-12 16:02:53 +0300

    used useState()

--- src/components/AddToDo/AddToDo.js
+++ src/components/AddToDo/AddToDo.js
@@ -1,22 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "../UI/Card";
 import classes from "./AddToDo.module.css";
 import Button from "../UI/Button";
 
 const AddToDo = (props) => {
+  const [enteredNum, setEnteredNum] = useState("");
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredDes, setEnteredDes] = useState("");
+
   const AddToDoHandler = (event) => {
     event.preventDefault();
+    console.log(enteredNum, enteredTitle, enteredDes);
   };
+
+  const numChangeHandler = (event) => {
+    setEnteredNum(event.target.value);
+  };
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const desChangeHandler = (event) => {
+    setEnteredDes(event.target.value);
+  };
+
   return (
     <Card className={classes.input}>
       <form onSubmit={AddToDoHandler}>
         <label htmlFor="number">No.</label>
-        <input id="no." type="number" />
+        <input
+          id="no."
+          type="number"
+          value={enteredNum}
+          onChange={numChangeHandler}
+        />
         <label htmlFor="title">Title</label>
-        <input id="title" type="text" />
+        <input
+          id="title"
+          type="text"
+          value={enteredTitle}
+          onChange={titleChangeHandler}
+        />
         <label htmlFor="description">Description</label>
-        <input id="description" type="text" />
+        <input
+          id="description"
+          type="text"
+          value={enteredDes}
+          onChange={desChangeHandler}
+        />
         <Button type="submit">Add To Do</Button>
       </form>
     </Card>
